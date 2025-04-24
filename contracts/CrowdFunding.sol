2bfed6664a941f8138a24ce7f3c23f8858552777
UnicornDad8 <unicorndad8@gmail.com>
2025-04-24 12:59:57 -0300

    La funcion crear campaña fue creada en CrowdFunding.sol

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -7,7 +7,7 @@ contract CrowdFunding {
         string title;
         string description;
         uint256 target;
-        uint256 deadeline;
+        uint256 deadline;
         uint256 amountCollected;
         string image;
         address[] donators;
@@ -17,4 +17,22 @@ contract CrowdFunding {
     mapping(uint256 => Campaign) public campaigns;
 
     uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
 }
